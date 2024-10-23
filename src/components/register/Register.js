cff8869ffc71e9c72ae22130623ccf8047eb9d40
Selva <sellva3009@gmail.com>
2024-10-23 15:39:23 +0530

    modifying folder structure

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -5,29 +5,7 @@ const Register = () => {
   // Implement registration logic here, this is just a placeholder
   return (
     <div className="container mt-5">
-      <h2 className="text-center">Register</h2>
-      <form>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input type="email" className="form-control" id="email" required />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-success">
-          Register
-        </button>
-      </form>
+      Register
     </div>
   );
 };
